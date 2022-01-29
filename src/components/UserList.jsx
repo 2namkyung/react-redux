7f03ace60f7e7b37942fd84ff7098aa59f564e29
2namkyung <iddbxk@daum.net>
2022-01-29 14:10:27 +0900

    refactoring async work, from presentation to container

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,21 +1,11 @@
-import axios from 'axios'
 import React, { useEffect } from 'react'
 
-export default function UserList({users, start, success, fail}){
+export default function UserList({users, getUsers}){
 
     useEffect(()=>{
-        async function getUsers(){
-            try{
-                start()
-                const response = await axios.get('https://api.github.com/users')
-                success(response.data)
-            }catch(error){
-                fail(error)
-            }
-        }
-
+        
         getUsers()
-    }, [start, success, fail])
+    }, [getUsers])
 
     if(users.length===0){
         return <p>현재 유저 정보 없음</p>
--- src/containers/UserListContainer.jsx
+++ src/containers/UserListContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector  } from 'react-redux'
+import axios from 'axios'
 import { getUsersFail, getUsersStart, getUsersSuccess } from '../actions'
 import UserList from '../components/UserList'
 
@@ -9,17 +9,15 @@ export default function UserListContainer(){
     const users = useSelector((state)=>state.users.data)
     const dispatch = useDispatch()
 
-    const start = useCallback(()=>{
-        dispatch(getUsersStart())
-    }, [dispatch])
+    const getUsers = useCallback(async function getUsers(){
+        try{
+            dispatch(getUsersStart())
+            const response = await axios.get('https://api.github.com/users')
+            dispatch(getUsersSuccess(response.data))
+        }catch(error){
+            dispatch(getUsersFail(error))
+        }
+    }, [])
 
-    const success = useCallback((data)=>{
-        dispatch(getUsersSuccess(data))
-    }, [dispatch])
-
-    const fail = useCallback((error)=>{
-         dispatch(getUsersFail(error))
-    }, [dispatch])
-
-    return (<UserList users={users} start={start} success={success} fail={fail}/>)
+    return (<UserList users={users} getUsers={getUsers}/>)
 }
